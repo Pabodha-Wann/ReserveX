94590b41d572a9e82db3e46bee3e5336ca8f03f7
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2026-08-31 02:30:39 +0530

    feat: add initial database schema for reservation and stall management tables

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,125 @@
+﻿-- MySQL dump 10.13  Distrib 9.2.0, for Win64 (x86_64)
+--
+-- Host: localhost    Database: reservex
+-- ------------------------------------------------------
+-- Server version	9.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reservation_genres`
+--
+
+DROP TABLE IF EXISTS `reservation_genres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservation_genres` (
+  `genre_name` varchar(255) NOT NULL,
+  `reservation_id` int NOT NULL,
+  `stall_id` int NOT NULL,
+  PRIMARY KEY (`genre_name`,`reservation_id`,`stall_id`),
+  KEY `FK3rg4lffyp8i05klh9bhjfhy8x` (`reservation_id`),
+  CONSTRAINT `FK3rg4lffyp8i05klh9bhjfhy8x` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`reservation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reservation_stalls`
+--
+
+DROP TABLE IF EXISTS `reservation_stalls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservation_stalls` (
+  `reservation_id` int NOT NULL,
+  `stall_id` int NOT NULL,
+  PRIMARY KEY (`reservation_id`,`stall_id`),
+  KEY `FK8tf7n64dngctuxi4phrsjjxle` (`stall_id`),
+  CONSTRAINT `FK4pj5jqqvyi0tekoml3ulfbbq0` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`reservation_id`),
+  CONSTRAINT `FK8tf7n64dngctuxi4phrsjjxle` FOREIGN KEY (`stall_id`) REFERENCES `stalls` (`stall_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `reservation_id` int NOT NULL AUTO_INCREMENT,
+  `qr_code_path` varchar(255) DEFAULT NULL,
+  `reservation_date` datetime(6) NOT NULL,
+  `status` enum('Approved','Pending','Rejected') NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`reservation_id`),
+  KEY `FKb5g9io5h54iwl2inkno50ppln` (`user_id`),
+  CONSTRAINT `FKb5g9io5h54iwl2inkno50ppln` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `stalls`
+--
+
+DROP TABLE IF EXISTS `stalls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stalls` (
+  `stall_id` int NOT NULL AUTO_INCREMENT,
+  `description` text,
+  `gridCol` int DEFAULT NULL,
+  `gridRow` int DEFAULT NULL,
+  `is_Confirmed` bit(1) DEFAULT NULL,
+  `stall_name` varchar(255) NOT NULL,
+  `price` double DEFAULT NULL,
+  `size` enum('large','medium','small') NOT NULL,
+  PRIMARY KEY (`stall_id`),
+  UNIQUE KEY `UKmmgfoo6f4t2kcdcr3urg3hd34` (`stall_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `auth0_sub` varchar(255) NOT NULL,
+  `business_name` varchar(255) DEFAULT NULL,
+  `created_at` datetime(6) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `last_updated_at` datetime(6) NOT NULL,
+  `no_of_current_bookings` int NOT NULL,
+  `role` enum('EMPLOYEE','VENDOR') NOT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UKgfc2lxchf8kon40q0tsem9vku` (`auth0_sub`),
+  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`),
+  UNIQUE KEY `UKr43af9ap4edm43mmtq01oddj6` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-08-31  2:05:39
